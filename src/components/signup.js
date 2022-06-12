ca9b0ca7d60253ad95e106d35fa8d910fbc053b5
eapenzacharias <eapenzac@gmail.com>
2022-06-12 23:33:58 +0200

    Connected Signup action with form

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,10 +1,45 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   Button, Container, FormControl, Link, TextField,
 } from '@mui/material';
+import { signUp } from '../store/users';
 import MyText from './typography';
 
 const SignUpForm = () => {
-  const userId = '';
+  const dispatch = useDispatch();
+
+  const [state, setState] = React.useState({
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+    date_of_birth: '',
+  });
+
+  const isSignedUp = useSelector(
+    (state) => state.usersReducer.currentUser.isSignedUp,
+  );
+
+  useEffect(() => {
+    if (isSignedUp) console.log('signed up');
+  }, [isSignedUp]);
+
+  const handleChange = (evt) => {
+    evt.preventDefault();
+    const { value } = evt.target;
+    setState({
+      ...state,
+      [evt.target.name]: value,
+    });
+  };
+
+  const submitForm = (evt) => {
+    evt.preventDefault();
+    dispatch(signUp(state));
+  };
+
   return (
     <>
       <Container
@@ -18,45 +53,49 @@ const SignUpForm = () => {
         }}
       >
         <MyText text="Sign Up" type="h4" />
-        <form>
+        <form onSubmit={submitForm}>
           <FormControl sx={{ width: '100%' }}>
             <TextField
+              name="name"
               margin="dense"
               required
-              id="standard-required"
               label="Name"
               variant="standard"
               type="text"
               autoComplete="Name"
+              onChange={handleChange}
             />
             <TextField
+              name="email"
               margin="dense"
               required
-              id="standard-required"
               label="Email"
               variant="standard"
               type="email"
               autoComplete="email"
+              onChange={handleChange}
             />
             <TextField
+              name="password"
               required
               margin="dense"
-              id="standard-password-input"
               label="Password"
               type="password"
               autoComplete="new-password"
               variant="standard"
+              onChange={handleChange}
             />
             <TextField
+              name="passwordConfirm"
               required
               margin="dense"
-              id="standard-password-input"
               label="Confirm Password"
               type="password"
               autoComplete="new-password"
               variant="standard"
+              onChange={handleChange}
             />
-            <Button variant="contained" sx={{ margin: '2rem 0' }}>Sign In</Button>
+            <Button type="submit" variant="contained" sx={{ margin: '2rem 0' }}>Sign Up</Button>
           </FormControl>
           <Link href="#signup" underline="always" sx={{ fontWeight: 500 }}>
             Login In
@@ -66,7 +105,6 @@ const SignUpForm = () => {
             Forgot your password?
           </Link>
         </form>
-        {userId}
       </Container>
     </>
   );
