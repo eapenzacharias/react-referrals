3f06d56622c2ee3ce2f73f815e9e916d6e6967eb
eapenzacharias <eapenzac@gmail.com>
2022-06-12 23:22:02 +0200

    Sign up action created in the reducer

--- src/store/users.js
+++ src/store/users.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import initialState from './initialState';
 
 const SIGN_IN = 'auth/signin';
+const SIGN_UP = 'auth/signup';
 
 const url = 'https://rails-referrals.herokuapp.com/api/auth/';
 
@@ -28,9 +29,34 @@ export const signIn = (user) => async (dispatch) => {
     });
 };
 
+export const signUp = (user) => async (dispatch) => {
+  const currentState = JSON.parse(JSON.stringify(initialState));
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  axios
+    .post(url, user, { headers })
+    .then((response) => {
+      console.log('Signed Up');
+      currentState.currentUser.isSignedIn = false;
+      currentState.currentUser.isSignedUp = true;
+      currentState.currentUser.attributes = response.data.data;
+      currentState.currentUser.headers = response.headers;
+      dispatch({
+        type: SIGN_UP,
+        payload: currentState,
+      });
+    })
+    .catch((error) => {
+      console.error('There was an error!', error);
+    });
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case SIGN_IN:
+      return action.payload;
+    case SIGN_UP:
       return action.payload;
     default:
       return state;
