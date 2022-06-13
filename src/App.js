9717f4880593577c9031ef895b0d74edf8bac0b1
eapenzacharias <eapenzac@gmail.com>
2022-06-13 20:30:53 +0200

    Updated navlinks depending on isSignedIn state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Container } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import React from 'react';
 import './assets/styles/App.css';
+import { useSelector } from 'react-redux';
 import LoginForm from './components/login';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -12,14 +13,22 @@ import Dashboad from './components/dashboard';
 import ResponsiveAppBar from './components/navbar';
 
 function App() {
+  const isSignedIn = useSelector(
+    (state) => state.usersReducer.currentUser.isSignedIn,
+  );
   return (
     <div className="App">
       <ResponsiveAppBar />
       <Container>
         <Routes>
-          <Route path="login" element={<LoginForm />} />
-          <Route path="signup" element={<SignUpForm />} />
-          <Route path="/" element={<Dashboad />} />
+          {!isSignedIn && (
+            <>
+              <Route path="login" element={<LoginForm />} />
+              <Route path="signup" element={<SignUpForm />} />
+              <Route path="/" element={<LoginForm />} />
+            </>
+          )}
+          {isSignedIn && (<Route path="/" element={<Dashboad />} />)}
         </Routes>
       </Container>
     </div>
