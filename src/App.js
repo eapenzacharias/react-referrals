391cd8f1676f52d714cc6219b0a84dbed30f8939
eapenzacharias <eapenzac@gmail.com>
2022-06-13 18:00:09 +0200

    Created dashboad component

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import { Routes, Route } from 'react-router-dom';
 import React from 'react';
 import './assets/styles/App.css';
 import LoginForm from './components/login';
-import Sample from './components/sample';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import SignUpForm from './components/signup';
+import Dashboad from './components/dashboard';
+import ResponsiveAppBar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <ResponsiveAppBar />
       <Container>
-        <Sample />
         <Routes>
           <Route path="login" element={<LoginForm />} />
           <Route path="signup" element={<SignUpForm />} />
+          <Route path="/" element={<Dashboad />} />
         </Routes>
       </Container>
     </div>
