49a9caa580769e27130936029a55b3cb0e759db8
eapenzacharias <eapenzac@gmail.com>
2022-06-14 11:21:47 +0200

    Show referral count on dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -7,13 +7,17 @@ import { useNavigate } from 'react-router-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 // import { signIn } from '../store/users';
 import MyText from './typography';
-import { sendInvite } from '../store/referrals';
+import { getReferrals, sendInvite } from '../store/referrals';
+// import ReferralTable from './referrals';
 
 const Dashboad = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const currentUser = useSelector((state) => state.usersReducer.currentUser);
+
+  const referralData = useSelector((state) => state.referralReducer);
+  console.log(referralData.referrals);
 
   const [state, setState] = React.useState({
     email: '',
@@ -22,6 +26,10 @@ const Dashboad = () => {
   const isSignedIn = useSelector(
     (state) => state.usersReducer.currentUser.isSignedIn,
   );
+
+  useEffect(() => {
+    dispatch(getReferrals(currentUser));
+  }, []);
 
   useEffect(() => {
     if (isSignedIn) {
@@ -49,7 +57,6 @@ const Dashboad = () => {
     <>
       <MyText text={`Hello ${currentUser.attributes.name}!`} type="h1" />
       <Container
-        className="centralized"
         margin="dense"
         maxWidth="xs"
         sx={{
@@ -61,9 +68,9 @@ const Dashboad = () => {
       >
         <MyText text="Invite your friends!" type="h4" />
         <MyText text="Share this link or send an invite." type="p" />
-        <MyText text={`http://localhost:3000/?ref=${currentUser.attributes.id}`} type="link" />
+        <MyText text={`http://localhost:3001/ref/${currentUser.attributes.id}`} type="link" />
         <CopyToClipboard
-          text={`http://localhost:3000/?ref=${currentUser.attributes.id}`}
+          text={`http://localhost:3001/ref/${currentUser.attributes.id}`}
           onCopy={() => alert('Copied')}
         >
           <Button variant="outlined" size="small">Copy Link</Button>
@@ -82,6 +89,7 @@ const Dashboad = () => {
             <Button type="submit" variant="contained" sx={{ margin: '2rem 0' }}>Send Invite</Button>
           </FormControl>
         </form>
+        <MyText text={`You have ${referralData.count} successful referral`} type="h4" />
       </Container>
     </>
   );
