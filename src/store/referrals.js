a98debbf7fbc19c10d3843d6376558d92464679a
eapenzacharias <eapenzac@gmail.com>
2022-06-14 11:25:10 +0200

    Get referrals action added

--- src/store/referrals.js
+++ src/store/referrals.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const SEND_INVITE = 'sendInvite';
+const GET_REFERRALS = 'referrals';
 
 // const url = 'https://rails-referrals.herokuapp.com/api/';
 const url = 'http://localhost:3000/api/';
@@ -22,9 +23,28 @@ export const sendInvite = (email, user) => async (dispatch) => {
     });
 };
 
+export const getReferrals = (user) => async (dispatch) => {
+  const currentState = {};
+  const { headers } = user;
+  axios.get(`${url}referrals`, { headers })
+    .then((response) => {
+      currentState.referrals = response.data.data;
+      currentState.count = response.data.count;
+      dispatch({
+        type: GET_REFERRALS,
+        payload: currentState,
+      });
+    })
+    .catch((error) => {
+      console.error('There was an error!', error);
+    });
+};
+
 const reducer = (state = [], action) => {
   switch (action.type) {
     case SEND_INVITE:
+      return action.payload;
+    case GET_REFERRALS:
       return action.payload;
     default:
       return state;
